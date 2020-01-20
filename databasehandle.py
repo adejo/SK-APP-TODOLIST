460212843be26934253a9d2d7f9be12a3f1e7381
adejo <adejoedoh4@gmail.com>
2020-01-20 18:13:01 +0100

    Create databasehandle.py

---
+++ databasehandle.py
@@ -0,0 +1,32 @@
+import requests
+import pyodbc
+
+
+def mysql_con():
+    conn = pyodbc.connect('Driver={SQL Server};'
+                      'Server=databaseserver;'
+                      'Database=mytodo;'
+                      'Trusted_Connection=yes;')
+
+    return conn
+
+
+def local_log(msdata):
+        conn = mysql_con()
+        corsor = conn.cursor()
+        query = """INSERT INTO ISM_KPI.dbo.mytodo (
+                        
+                        [todo_name],
+                        [date],
+                        [desc],
+                        
+                        ) values"""
+
+
+        try:
+            corsor.execute(query + str(msdata))
+            corsor.close()
+            conn.commit()
+            return True
+        except Exception as e:
+            return str(e)
